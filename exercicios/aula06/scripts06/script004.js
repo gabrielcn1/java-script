77bbefd05c8309e36805230846562b72ec306106
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-13 19:58:37 -0300

    att

---
+++ exercicios/aula06/scripts06/script004.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome?')
+document.write(`Olá, ${nome}. Seu nome tem ${nome.length} letras. <br/>`)
+document.write(`Seu nome em maiúscula é ${nome.toUpperCase()} <br/>`)
+document.write(`Seu nome em minúscula é ${nome.toLowerCase()}`)
