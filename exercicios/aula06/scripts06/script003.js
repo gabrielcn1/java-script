32813db640cc0e426b20b08d4c064f57a3b63a66
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-13 21:25:59 -0300

    att

--- exercicios/aula06/scripts06/script003.js
+++ exercicios/aula06/scripts06/script003.js
@@ -3,5 +3,6 @@ var n2 = Number(window.prompt('Digite outro número: '))
 var s = n1 + n2
 
 window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`)
+        //  ('A soma entre ' + n1 + ' e ' + n2 + ' é igaul a ' + s)
 // (Number + Number) para adição
 // (String + String) para concatenaçã 
