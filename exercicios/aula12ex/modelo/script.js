93a39391c79897a6aba7e38b438ec58c20e6db34
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-20 16:42:08 -0300

    ex12

---
+++ exercicios/aula12ex/modelo/script.js
@@ -0,0 +1,27 @@
+// DOM
+var corpo = document.getElementById('body')
+var hea = document.getElementById('header')
+var secao = document.getElementById('section')
+var rodape = document.getElementById('footer') 
+
+// Estilo
+corpo.style.background = 'rgb(107, 107, 241'
+corpo.style.font = 'normal 15pt Arial'
+
+hea.style.color = 'white'
+hea.style.textAlign = 'center'
+
+secao.style.background = 'white'
+secao.style.borderRadius = '10px'
+secao.style.padding = '15px'
+secao.style.width = '500px'
+secao.style.margin = 'auto'
+secao.style.boxShadow = '3px 3px 10px rgba(0, 0, 0, 0.363)'
+
+rodape.style.color = 'white'
+rodape.style.textAlign = 'center'
+rodape.style.fontStyle = 'italic'
+
+// Script
+
+alert('Olá!')
