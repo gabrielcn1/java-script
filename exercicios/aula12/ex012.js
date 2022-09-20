952a1b0648b1261c8746e9fd8eba0acf0543b264
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-20 01:41:43 -0300

    att

--- exercicios/aula12/ex012.js
+++ exercicios/aula12/ex012.js
@@ -1,11 +1,12 @@
-var hora = 11
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12 ) {
     console.log('Bom dia')
 } else if (hora <= 18) {
     console.log('Boa tarde')
-} else if (hora <= 24){
+} else if (hora <= 24) {
     console.log('Boa noite')
 } else {
     console.log('Boa madrugada')
-}
+} 
