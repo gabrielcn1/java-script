2ce4c3090e48c299c6248e4b51bf1f2f2c1b622c
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-20 17:00:42 -0300

    att

--- exercicios/aula12ex/modelo/ex014/script.js
+++ exercicios/aula12ex/modelo/ex014/script.js
@@ -6,9 +6,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.png'
+        document.body.style.background = 'rgb(102, 103, 105)'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'tarde.png'
+        document.body.style.background = 'rgb(126, 67, 27)'
     } else {
         img.src = 'noite.png'
+        document.body.style.background = 'rgb(25, 24, 24)'
     }
 }
