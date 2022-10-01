b2b7eefac5eb236d632a78f23eec545ad2dc8d31
Gabriel Costa <gabrielcn03@outlook.com>
2022-09-30 21:27:27 -0300

    att

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,8 +1,9 @@
-var c = 1
+/* var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
+*/
 
 /*
 var c = 1
@@ -11,3 +12,23 @@ while (c <= 6) {
     c++
 }
 */
+
+/* 
+var x = 2
+while (x <= 8) {
+    console.log(`Número ${x}`)
+    x++
+}
+*/
+
+/*
+var x = 1
+while(x <= 12) {
+    console.log(`Número ${x}`)
+    x++
+}
+*/
+
+for (var x=1; x<=10; x++) {
+    console.log (`Teste ${x}`)
+}
