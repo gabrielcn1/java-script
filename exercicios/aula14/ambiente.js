313737333489fd74a9f7c273915dd20853641d03
Gabriel Costa <gabrielcn03@outlook.com>
2022-10-02 23:14:52 -0300

    att

--- exercicios/aula14/ambiente.js
+++ exercicios/aula14/ambiente.js
@@ -1,5 +1,5 @@
-console.log('Vai começar...')
-for (var c = 1;c <= 10;c++) {
-    console.log(`Teste ${c}`)
+console.log('Vou contar de 1 até 20')
+for (var x = 1; x <= 20; x++) {
+    console.log(`Número ${x}`)
 }
-console.log('FIM!!!')
+console.log('Contei até 20')
