1615d98a0f796b6a204d09ec2e7cc807278db9e7
Gabriel Costa <gabrielcn03@outlook.com>
2022-10-11 00:11:44 -0300

    att

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor é ${num[0]}`)
+let pos = num.indexOf(3)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
+  
