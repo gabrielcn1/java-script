8622ffa111d24d650963324f4485dd1707051e3b
Gabriel Costa <gabrielcn03@outlook.com>
2022-10-17 21:05:29 -0300

    att

--- exercicios/aula16ex/modelo/script.js
+++ exercicios/aula16ex/modelo/script.js
@@ -21,7 +21,10 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
